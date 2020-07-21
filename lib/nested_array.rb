3f0f16f4919c4d5d25af0008eab33d31d9d58061
MARIUS MOLDOVAN <biris_21@yahoo.com>
2020-07-21 23:42:13 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -42,7 +42,7 @@ def matrix_lookup(matrix, row, column)
     ["Strawberries", "Potatoes",  "Grapes",   "Avocadoes", "Asparagus"]
   ]
 
-  storage_room[1][1]
+  storage_room[0][1]
   # Given any matrix (array of arrays), a row index and a column index,
   # Return the matrix's content at that row and and column
 end
